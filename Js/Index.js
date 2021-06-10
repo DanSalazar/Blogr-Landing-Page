6745ff1cdfd4e3b11bbe40036da328a690652068
Dan <danielsalazar.cc@gmail.com>
2021-06-09 21:10:39 -0400

    Nav js

--- Js/Index.js
+++ Js/Index.js
@@ -1,5 +1,5 @@
 const navList = document.querySelectorAll('.nav__title');
-const navSubList = document.querySelectorAll('.nav__sublist');
+const navSubNavigation = document.querySelectorAll('.nav__sublist');
 const iconsNav = document.querySelectorAll('.nav__icon');
 const hambuger = document.querySelector('#hamburger');
 const nav = document.querySelector('.nav');
@@ -11,19 +11,15 @@ hambuger.addEventListener('click', () => {
 
 navList.forEach((element, index) => {
     element.addEventListener('click', () => {
-        navSubList[index].classList.add('show');
+        navSubNavigation[index].classList.add('show');
         
-        for(let i = 0; i < navSubList.length; i++){
+        for(let i = 0; i < navSubNavigation.length; i++){
             if( index !== i ){
-                navSubList[i].classList.remove('show');
+                navSubNavigation[i].classList.remove('show');
                 iconsNav[i].classList.remove('reverse');
             }
         }
 
         iconsNav[index].classList.add('reverse');
     })
-})
-
-document.body.addEventListener('click', () => {
-    console.log("pitooo")
 })
